234823affbe88a1a3d688844567b09ba6828f684
GotoLink <gotolinkminecraft@gmail.com>
2013-05-14 21:54:49 +0200

    Added proxies.Simplified config.Registered items.

--- Minecessity.java
+++ Minecessity.java
@@ -1,122 +1,115 @@
 package mods.minecessity;
 
-import java.io.File;
-import java.util.Map;
-
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockTorch;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.src.ModLoader;
-import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.WorldType;
+import net.minecraftforge.common.Configuration;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.EntityRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
-@Mod(modid = "minecessity",version="alpha")
+@Mod(modid = "minecessity",name="Minecessity",version="alpha")
 @NetworkMod(clientSideRequired=true,serverSideRequired=false)
-public class Minecessity
+public class Minecessity 
 {
 	@Instance("minecessity")
 	public static Minecessity instance;
-	@SidedProxy()
+	@SidedProxy(clientSide="mods.minecessity.ClientProxy",serverSide="mods.minecessity.CommonProxy")
 	public static CommonProxy proxy;
-	public static String dir = (new StringBuilder()).append(Minecraft.getMinecraftDir()).append("/").append("Minecessity.properties").toString();
-    public static Y_PropFile props=new Y_PropFile((new File(dir)).getPath());
-    public static Block table,mobAttractor,projDeflector,particleBlock,chair,ceilLamp;
+	public static Block table,mobAttractor,projDeflector,particleBlock,chair,ceilLamp;
 	public static BlockTorch tempTorch;
-	public static Item tableItem,cactusStick,portableWorkBench,particleGun,chairItem,ceilLampItem;
-	public static void prepareProps()
+	public static Item tableItem,chairItem,ceilLampItem;
+	public static Item cactusStick,portableWorkBench,particleGun;
+	public static int tableBlockId,mobAttractorId,temporaryTorchId,deflectorId,particleBlockId,
+	chairBlockId,ceilLampBlockId,tableItemId,cactusPickerId,portableWorkBenchId,particleGunId,
+	chairItemId,ceilLampItemId,deflectorEffectiveRange,slimeSpawnLimit;
+	private Configuration config;
+	
+	@PreInit
+	public void prepareProps(FMLPreInitializationEvent event)
 	{
-		props.getInt("Table block ID",110);
-		props.getInt("Mob Attractor block ID",111);
-		props.getInt("Temporary Torch block ID",112);
-		props.getInt("Deflector block ID",113);
-		props.getInt("Particle block ID",114);
-		props.getInt("Chair block ID",115);
-		props.getInt("Celing Lamp blok ID",116);
+		config =new Configuration(event.getSuggestedConfigurationFile());
+		config.load();
+		tableBlockId =config.get("block","Table ID",410).getInt();
+		mobAttractorId =config.get("block","Mob Attractor ID",411).getInt();
+		temporaryTorchId =config.get("block","Temporary Torch ID",412).getInt();
+		deflectorId =config.get("block","Deflector ID",413).getInt();
+		particleBlockId =config.get("block","Particle block ID",414).getInt();
+		chairBlockId =config.get("block","Chair ID",415).getInt();
+		ceilLampBlockId =config.get("block","Ceiling Lamp ID",416).getInt();
 		
-		props.getInt("Table item ID",26508);
-		props.getInt("Cactus Picker item ID",26509);
-		props.getInt("Portable Workbench item ID",26510);
-		props.getInt("Particles Gun item ID",26511);
-		props.getInt("Chair item ID",26512);
-		props.getInt("Celing Lamp item ID",26513);
+		tableItemId =config.get("item","Table ID",26508).getInt();
+		cactusPickerId =config.get("item","Cactus Picker ID",26509).getInt();
+		portableWorkBenchId =config.get("item","Portable Workbench ID",26510).getInt();
+		particleGunId =config.get("item","Particles Gun ID",26511).getInt();
+		chairItemId =config.get("item","Chair ID",26512).getInt();
+		ceilLampItemId =config.get("item","Ceiling Lamp ID",26513).getInt();
 		
-		props.getInt("Deflector Effective Range",8);
-		props.getInt("Slime spawn limit each chunk",3);
+		deflectorEffectiveRange =config.get("general","Deflector Effective Range",8).getInt();
+		slimeSpawnLimit=config.get("general","Slime spawn limit by chunk",3).getInt();
+		if(config.hasChanged())
+			config.save();
 	}
-	
-	public static int rendererTable,rendererChair,rendererCeilLamp;
-	
-	public static int TableBlockId = props.getInt("Table block ID");
-	public static int MobAttractorId = props.getInt("Mob Attractor block ID");
-	public static int TemporaryTorchId = props.getInt("Temporary Torch block ID");
-	public static int DeflectorId = props.getInt("Deflector block ID");
-	public static int ChairBlockId = props.getInt("Chair block ID");
-	public static int ParticleBlock = props.getInt("Particle block ID");
-	public static int CeilLampBlockId = props.getInt("Celing Lamp blok ID");
-	
-	public static int TableItemId = props.getInt("Table item ID");
-	public static int CactusPickerId = props.getInt("Cactus Picker item ID");
-	public static int PortableWorkBenchId = props.getInt("Portable Workbench item ID");
-	public static int ParticleGunId = props.getInt("Particles Gun item ID");
-	public static int ChairItemId = props.getInt("Chair item ID");
-	public static int CeilLampItemId = props.getInt("Celing Lamp item ID");
-	
-	public static int DeflectorEffectiveRange = props.getInt("Deflector Effective Range");
-	
-    protected Minecessity()
+	@Init
+    public void load(FMLInitializationEvent event)
     {
-    	prepareProps();
-
-    	table = new Y_BlockTable(TableBlockId).setHardness(0.5F).setName("minecessity:table");
-    	mobAttractor = new Y_BlockMobAttract(MobAttractorId).setHardness(0.5F).setName("minecessity:mobAttractor");
-    	tempTorch = (BlockTorch)(new Y_BlockTempTorch(TemporaryTorchId)).setLightValue(0.9375F).setName("tempTorch");
-    	projDeflector = new Y_BlockProjectDeflector(DeflectorId).setHardness(0.85F).setName("minecessity:deflector");
-    	particleBlock = new Y_BlockParticle(ParticleBlock).setHardness(0.4F).setName("minecessity:particleBlock");
-    	chair = new Y_BlockChair(ChairBlockId).setHardness(0.5F).setName("minecessity:chair");
-    	ceilLamp = new Y_BlockCeilLamp(CeilLampBlockId).setLightValue(1F).setHardness(0.5F).setName("minecessity:ceilLamp");
+    	table = new BlockTable(tableBlockId).setUnlocalizedName("wood");
+    	mobAttractor = new BlockMobAttract(mobAttractorId).setHardness(0.5F).setUnlocalizedName("minecessity:mobAttractor");
+    	tempTorch = (BlockTorch)(new BlockTempTorch(temporaryTorchId)).setLightValue(0.9375F).setUnlocalizedName("torch");
+    	projDeflector = new BlockProjectDeflector(deflectorId).setHardness(0.85F).setUnlocalizedName("minecessity:deflector");
+    	particleBlock = new BlockParticle(particleBlockId).setHardness(0.4F).setUnlocalizedName("minecessity:particleBlock");
+    	chair = new BlockChair(chairBlockId).setUnlocalizedName("wood");
+    	ceilLamp = new BlockCeilLamp(ceilLampBlockId).setUnlocalizedName("wood");
     	
-    	tableItem = (new Y_ItemTable(TableItemId)).setName("tableItem");
-    	cactusStick = (new Y_ItemCactusStick(CactusPickerId)).setName("minecessity:cactusPicker");
-    	portableWorkBench = (new Y_ItemPrtbWorkBence(PortableWorkBenchId)).setName("minecessity:portableWorkBench");
-    	particleGun = (new Y_ItemParticleGun(ParticleGunId)).setName("minecessity:particleGun");
-    	chairItem = (new Y_ItemChair(ChairItemId)).setName("chairItem");
-    	ceilLampItem = (new Y_ItemCeilLamp(CeilLampItemId)).setName("ceilLampItem");
-		//RenderingRegistry.registerBlockHandler();
-		/*rendererTable = ModLoader.getUniqueBlockModelID(this, false);
-		rendererChair = ModLoader.getUniqueBlockModelID(this, false);
-		rendererCeilLamp = ModLoader.getUniqueBlockModelID(this, false);*/
-		
-		RegisterBlocksItemsRecipes();
-		
+    	tableItem = new MagicItem(tableItemId,tableBlockId).setUnlocalizedName("minecessity:table");
+    	cactusStick = new ItemCactusStick(cactusPickerId).setUnlocalizedName("minecessity:cactusPicker");
+    	portableWorkBench = new ItemPrtbWorkBence(portableWorkBenchId).setUnlocalizedName("minecessity:portableWorkBench");
+    	particleGun = new ItemParticleGun(particleGunId).setUnlocalizedName("minecessity:particleGun");
+    	chairItem = new MagicItem(chairItemId,chairBlockId).setUnlocalizedName("minecessity:chair");
+    	ceilLampItem = new ItemCeilLamp(ceilLampItemId,ceilLampBlockId).setUnlocalizedName("minecessity:ceilLamp");
+    	
+    	registerBlocksItemsRecipes();
+    	proxy.registerRenderer();
+    	EntityRegistry.registerModEntity(Y_EntitySlime.class, "YSlime",1,this,40,1,true);
 		EntityRegistry.addSpawn(Y_EntitySlime.class, 1, 1, 5, EnumCreatureType.monster,WorldType.base12Biomes);
-		EntityRegistry.registerModEntity(Y_EntitySlime.class, "YSlime",1,this,0,0,false);
 		
-		GameRegistry.registerTileEntity(Y_TileEntityParticleBlock.class,"Particle Block Tile Entity");
+		GameRegistry.registerTileEntity(TileEntityParticleBlock.class,"Particle Block Tile Entity");
+	
 	}
 	
-	public void AddRenderer(Map map)
-    {
-        map.put(Y_EntityLightBullet.class, new Y_RenderLightBullet());
-    }
-	
-	public void RegisterBlocksItemsRecipes()
+	public void registerBlocksItemsRecipes()
 	{
-		GameRegistry.registerBlock(table,"Table");
-		LanguageRegistry.instance().addNameForObject(table,"en_US","Table");
+		GameRegistry.registerItem(tableItem, "Table");
+		//GameRegistry.registerBlock(table,"Table");
 		LanguageRegistry.instance().addNameForObject(tableItem,"en_US","Table");
 		GameRegistry.addRecipe(new ItemStack(tableItem,1),new Object[]{
 			"XLX"," Y "," X ", 'X',Block.planks , 'L',Block.lever , 'Y',Item.ingotIron
+		});
+		
+		GameRegistry.registerItem(chairItem, "Chair");
+		//GameRegistry.registerBlock(chair,"Chair");
+		LanguageRegistry.instance().addNameForObject(chairItem,"en_US","Chair");
+		GameRegistry.addRecipe(new ItemStack(chairItem,1),new Object[]{
+			" W "," LI"," WW", 'W',Block.planks , 'L',Block.lever , 'I',Item.ingotIron
+		});
+		
+		GameRegistry.registerItem(ceilLampItem, "Ceiling Lamp");
+		//GameRegistry.registerBlock(ceilLamp,"Ceiling Lamp");
+		LanguageRegistry.instance().addNameForObject(ceilLampItem,"en_US","Ceiling Lamp");
+		GameRegistry.addRecipe(new ItemStack(ceilLampItem,1),new Object[]{
+			" W ", " L ", "GIG" , 'W',Block.planks, 'L',Block.lever, 'G',Block.glowStone, 'I',Item.ingotIron
 		});
 		
 		GameRegistry.registerBlock(mobAttractor,"Mob Attractor");
@@ -145,32 +138,20 @@ public class Minecessity
 		GameRegistry.addRecipe(new ItemStack(particleBlock,1),new Object[]{
 			"CGC", "CDC", "CCC" , 'C',Block.cobblestone, 'G',Block.glass, 'D',particleGun
 		});
-
-		GameRegistry.registerBlock(chair,"Chair");
-		LanguageRegistry.instance().addNameForObject(chair,"en_US","Chair");
-		LanguageRegistry.instance().addNameForObject(chairItem,"en_US","Chair");
-		GameRegistry.addRecipe(new ItemStack(chairItem,1),new Object[]{
-			" W "," LI"," WW", 'W',Block.planks , 'L',Block.lever , 'I',Item.ingotIron
-		});
-		GameRegistry.registerBlock(ceilLamp,"Ceiling Lamp");
-		LanguageRegistry.instance().addNameForObject(ceilLamp,"en_US","Ceiling Lamp");
-		LanguageRegistry.instance().addNameForObject(ceilLampItem,"en_US","Ceiling Lamp");
-		GameRegistry.addRecipe(new ItemStack(ceilLampItem,1),new Object[]{
-			" W ", " L ", "GIG" , 'W',Block.planks, 'L',Block.lever, 'G',Block.glowStone, 'I',Item.ingotIron
-		});
 		
-		
-		
+		GameRegistry.registerItem(cactusStick,"Cactus Picker");
 		LanguageRegistry.instance().addNameForObject(cactusStick,"en_US","Cactus Picker");
 		GameRegistry.addRecipe(new ItemStack(cactusStick,1),new Object[]{
 			"X","X","S" , 'X',Block.cactus , 'S',Item.stick
 		});
 		
+		GameRegistry.registerItem(portableWorkBench,"Portable Workbench");
 		LanguageRegistry.instance().addNameForObject(portableWorkBench,"en_US","Portable Workbench");
 		GameRegistry.addRecipe(new ItemStack(portableWorkBench,1),new Object[]{
 			"ILI", "RWR", "IRI", 'W', Block.workbench , 'I' , Item.ingotIron , 'R' , Item.redstone , 'L' , Block.lever
 		});
 		
+		GameRegistry.registerItem(particleGun,"Particles Gun");
 		LanguageRegistry.instance().addNameForObject(particleGun,"en_US","Particles Gun");
 		GameRegistry.addRecipe(new ItemStack(particleGun,1),new Object[]{
 			"123", " 45", " 6I" , 'I',Item.ingotIron, '1',new ItemStack(Item.dyePowder,1), '2',new ItemStack(Item.dyePowder,2)
@@ -178,42 +159,24 @@ public class Minecessity
 			, '6',new ItemStack(Item.dyePowder,15)
 		});
     }
-	
+	@Deprecated
 	public boolean OnTickInGame(Minecraft minecraft)
     {
 		EntityPlayer player = minecraft.thePlayer;
 		
-		int x = DeflectorEffectiveRange;
+		int x = deflectorEffectiveRange;
 		for(int i = (int)Math.floor(player.posX)-x ; i <(int)Math.floor(player.posX)+x ; i++){
 		for(int j = (int)Math.floor(player.boundingBox.minY)-x ; j <(int)Math.floor(player.boundingBox.maxY)+x ; j++){
 		for(int k = (int)Math.floor(player.posZ)-x ; k <(int)Math.floor(player.posZ)+x ; k++){
 		
 			if(minecraft.theWorld.getBlockId(i,j,k)==projDeflector.blockID) 
-				((Y_BlockProjectDeflector)projDeflector).deflectProjectiles(minecraft.theWorld,i,j,k);
+				((BlockProjectDeflector)projDeflector).deflectProjectiles(minecraft.theWorld,i,j,k);
 			
 		}}}
         return true;
     }
 	
-	public boolean RenderWorldBlock(RenderBlocks renderblocks, IBlockAccess iblockaccess, int i, int j, int k, Block block, int l) 
-	{
-        if (l == rendererTable) 
-		{
-            new Y_NewRender().renderTable(renderblocks, iblockaccess, i, j, k, block);
-            return true;
-        }
-		if (l == rendererChair) 
-		{
-            new Y_NewRender().renderChair(renderblocks, iblockaccess, i, j, k, block);
-            return true;
-        }
-		if (l == rendererCeilLamp) 
-		{
-            new Y_NewRender().renderCeilLamp(renderblocks, iblockaccess, i, j, k, block);
-            return true;
-        }
-        return false;
-    }
+	
 	
 	
 }
